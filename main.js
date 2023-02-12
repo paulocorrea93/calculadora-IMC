9130e697f5d87fc6e93993074198f4b58bf494b0
Paulo <paulocorreapc93@gmail.com>
2023-02-12 11:31:55 -0400

    adicionada a validação de informações  do usuário

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 function imc (){
+    
     const nome = document.getElementById('nome').value;
     const peso = document.getElementById('peso').value;
     const altura = document.getElementById('altura').value;
     const resultado = document.getElementById('resultado');
+
     
     const calcIMC = ((peso) / (altura * altura)).toFixed(2);
-    
-    resultado.textContent = `Olá ${nome}, seu IMC é ${calcIMC}!`
+
+    if (nome != '' && altura != '' && peso != ''){
+        
+        let categoria = '';
+        
+        if(calcIMC < 18){
+            categoria = 'você está abaixo do peso ideal';
+        } else if (calcIMC < 25){
+            categoria = 'você está no peso ideal';
+        } else if (calcIMC < 30){
+            categoria = 'você está com sobrepeso';
+        } else {
+            categoria = 'você está obeso!';
+        }
+        
+        resultado.textContent = `Olá ${nome}, seu IMC é ${calcIMC} e ${categoria}!`
+    } else{
+        resultado.textContent = 'Preencha todos os campos!'
+    }
+
+
 }
 
 const calcular = document.getElementById('calcular');
